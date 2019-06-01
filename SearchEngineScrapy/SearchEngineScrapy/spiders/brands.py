cef891b597d04f128e4b2eaf97361d6a126d802b
root <root@e2e-39-21.e2enetworks.net.in>
2019-06-01 11:17:29 +0530

    added

--- SearchEngineScrapy/SearchEngineScrapy/spiders/brands.py
+++ SearchEngineScrapy/SearchEngineScrapy/spiders/brands.py
@@ -10,7 +10,7 @@ import json
 class BrandsSpider(scrapy.Spider):
     name = 'brands'
     allowed_domains = ['http://www.oriparts.com']
-    loc = ('../MarutiAutoParts/Ciaz/Ciaz-3-2986.xlsx')
+    loc = ('../MarutiAutoParts/Ciaz/Ciaz-1-2990.xlsx')
     rotate_user_agent = True
     wb = xlrd.open_workbook(loc)
     sheet = wb.sheet_by_index(0)
@@ -20,7 +20,7 @@ class BrandsSpider(scrapy.Spider):
         if len(sheet.cell_value(i, 6)) > 15:
             data.append(sheet.cell_value(i, 6))
     print(len(data[2000:]))
-    start_urls = [i for i in data[1500:]]
+    start_urls = [i for i in data[0:1500]]
     # start_urls = ['https://boodmo.com/catalog/part-nut-6767270/','https://boodmo.com/catalog/part-switch_assy_lighting_turn-6773719/',
     #               'https://boodmo.com/catalog/part-spark_plug-6772555/',
     #               'https://boodmo.com/catalog/part-cushion_glove_box_side-6787085/'
